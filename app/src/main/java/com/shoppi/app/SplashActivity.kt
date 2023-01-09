8a7da133b767e566469137ecb2ba8bc8d6489687
jaeYoun <rlawodusx@naver.com>
2023-01-09 21:01:46 +0900

    [feat: 실습 1. 스플래시 화면 구현 & 앱 호환성]

---
+++ app/src/main/java/com/shoppi/app/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.shoppi.app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 스플래시 액티비티 구현
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
