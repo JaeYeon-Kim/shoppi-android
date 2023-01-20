4cb7507268332ae1b69a0b01e4d58b863c849b56
jaeYoun <rlawodusx@naver.com>
2023-01-20 22:39:51 +0900

    [feat: Viewpager - Json 데이터 파싱 with gson]

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -10,7 +10,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayout
+import com.google.gson.Gson
 import org.json.JSONObject
 
 class HomeFragment: Fragment() {
@@ -29,23 +32,33 @@ class HomeFragment: Fragment() {
         // 텍스트뷰와 이미지뷰 변수 생성
         val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
         val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
+        val viewpager = view.findViewById<ViewPager2>(R.id.viewpager_home_banner)
+        val viewpagerIndicator = view.findViewById<TabLayout>(R.id.viewpager_home_banner_indicator)
+
 
         // json 변환을 위해 만들어둔 클래스 인스턴스 생성
         val assetLoader = AssetLoader()
-        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
-        Log.d("homeData", homeData ?: "")
+        val homeJsonString = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeJsonString ?: "")
 
         // homeData를 JsonObject로 변환하기, homeData가 null이거나 empty가 아닐때에만 JsonObject 생성
-        if(!homeData.isNullOrEmpty()) {
-            val jsonObject = JSONObject(homeData)
-            val title = jsonObject.getJSONObject("title")
-            val text = title.getString("text")
-            val iconUrl = title.getString("icon_url")
+        if(!homeJsonString.isNullOrEmpty()) {
+            // gson 사용
+            val gson = Gson()
+            val homeData = gson.fromJson(homeJsonString, HomeData::class.java)
             // 앱바 텍스트뷰 변경
-            toolbarTitle.text = text
+            toolbarTitle.text = homeData.title.text
             GlideApp.with(this)
-                .load(iconUrl)
+                .load(homeData.title.iconUrl)
                 .into(toolbarIcon)
+
+
+            // 뷰페이저 어댑터 할당
+            viewpager.adapter = HomeBannerAdapter().apply{
+                // 배너의 리스트를 전달
+                submitList(homeData.topBanners)
+            }
+
         }
     }
 }
